3897d6fbbbeea1a4a240530cef8725445fc262ac
Zachary Williams <zachary.williams@stjschools.org>
2023-04-14 13:22:08 -0500

    Added Digital Clock

--- js/clock.js
+++ js/clock.js
@@ -1,3 +1,7 @@
+//Variabless
+const secondHand = document.querySelector('.second-hand');
+const minsHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
 //Functions
 
 function setDigital() {
@@ -37,11 +41,28 @@ function setHex() {
     elm.innerHTML = s;
 }
 
+function rotate() {
+    const now = new Date();
+
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+    minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+    const hour = now.getHours();
+    const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+}
+
 function display() {
     setDigital();
     setBinary();
     setOctal();
     setHex();
+    rotate();
 }
 
 setInterval(display, 1000);
